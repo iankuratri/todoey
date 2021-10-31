f445006790800a20706e5fa7502954602ce2f67e
Ankur Atri <ankuratri.in@gmail.com>
2021-10-31 22:17:12 +0530

    filename extension changed to jsx for better intellisense

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <h1>This is a todo app!</h1>;
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <h1>This is a todo app!</h1>;
+  }
+}
+
+export default App;
