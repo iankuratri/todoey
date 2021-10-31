4a02e60146f9b0308ed4763302ddfcfb0ec20f84
Ankur Atri <ankuratri.in@gmail.com>
2021-10-31 22:37:43 +0530

    basic html structure added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ import React from "react";
 
 class App extends React.Component {
   render() {
-    return <h1>This is a todo app!</h1>;
+    return (
+      <main>
+        <header>A logo and Clear all button.</header>
+
+        <section>Two form fields and Add button.</section>
+
+        <article>Filter buttons and list of todo items.</article>
+      </main>
+    );
   }
 }
 
