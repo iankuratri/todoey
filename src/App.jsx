c15b0c3ffaf196ff8eb68da5b1e55e9c42d6b5d8
Ankur Atri <ankuratri.in@gmail.com>
2021-11-19 16:20:59 +0530

    App class component changed to function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import AddTodoForm from "./components/AddTodoForm";
 import FilterTodo from "./components/FilterTodo";
 import ListTodo from "./components/ListTodo";
 import todoService from "./services/todoService";
 
+// Function component
+
+const App = () => {
+  const savedTodos = todoService.getTodos();
+  const [todos, setTodos] = useState(savedTodos);
+  const [selectedFilter, setSelectedFilter] = useState("all");
+
+  const handleAddTodo = (todo) => {
+    const newTodo = { ...todo, id: Date.now(), completed: false };
+    const updatedTodos = [newTodo, ...todos];
+
+    todoService.saveTodos(updatedTodos);
+    setTodos(updatedTodos);
+  };
+
+  const handleClearAll = () => {
+    todoService.deleteAllTodos();
+    setTodos([]);
+  };
+
+  const handleFilterChange = ({ filteredTodos, newFilter }) => {
+    setSelectedFilter(newFilter);
+    setTodos(filteredTodos);
+  };
+
+  const handleUpdatedTodos = (todos) => {
+    todoService.saveTodos(todos);
+    setTodos(todos);
+  };
+
+  return (
+    <main className="container">
+      <Header onClearAll={handleClearAll} />
+      <AddTodoForm onAddTodo={handleAddTodo} />
+
+      {!!savedTodos.length && (
+        <FilterTodo
+          savedTodos={savedTodos}
+          selectedFilter={selectedFilter}
+          onFilterChange={handleFilterChange}
+        />
+      )}
+
+      <ListTodo todos={todos} onUpdate={handleUpdatedTodos} />
+    </main>
+  );
+};
+
+// Class component
+
+/**
+ 
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -56,5 +108,7 @@ class App extends React.Component {
     );
   }
 }
+
+*/
 
 export default App;
