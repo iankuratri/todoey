8da0d38ffc21ae730838b643efedd7f7fc25d849
Ankur Atri <ankuratri.in@gmail.com>
2021-11-19 16:24:24 +0530

    Header class component changed to function component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 
+// Function component
+
+const Header = ({ onClearAll }) => {
+  return (
+    <header className="block block-header">
+      <h1 className="header__brand">Todoey!</h1>
+      <button className="btn btn--primary" onClick={onClearAll}>
+        Clear All
+      </button>
+    </header>
+  );
+};
+
+// Class component
+
+/** 
+
 class Header extends React.Component {
   render() {
     const { onClearAll } = this.props;
@@ -14,5 +31,7 @@ class Header extends React.Component {
     );
   }
 }
+
+*/
 
 export default Header;
